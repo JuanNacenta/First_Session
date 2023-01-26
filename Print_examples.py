1971c57d432078ee68170dd6f514e6e0e4f3bf2f
JuanNacenta <juannfmg@gmail.com>
2023-01-26 11:01:18 +0100

    Initial commit

---
+++ Print_examples.py
@@ -0,0 +1,8 @@
+print("this is a text")
+print("hey muy buenas a todos aqui jaggermaister 777, comentando en...")
+print( "this", "can", "be", "written", "like", "this",2+2,"an there is a operatin 2+2")
+print("this", "can", "be", "written", "like", "this",sep="!")
+print("this", "can", "be", "written", "like", "this",sep="pito pito pito")
+print("this is a text", end="i want this at the end but deletes the enter")
+print("but as can you see know is together", end="now i cana add also the enter\n")
+print("ahora no esta junto jiji, coso rojos siempre despues de los verdes")
